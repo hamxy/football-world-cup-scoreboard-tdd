e8ddb5c9783e9938f20212a8356967d6c9a8b83e
hamxy <zylkadamian@gmail.com>
2025-06-20 23:59:02 +0200

    RED: Add failing test shouldSortGamesByTotalScoreAndRecency

--- src/test/java/ScoreBoardTest.java
+++ src/test/java/ScoreBoardTest.java
@@ -69,4 +69,26 @@ public class ScoreBoardTest {
         assertEquals(0, scoreBoard.getSummary().size());
     }
 
+    @Test
+    public void shouldSortGamesByTotalScoreAndRecency() {
+        scoreBoard.startGame("Mexico", "Canada");
+        scoreBoard.startGame("Spain", "Brazil");
+        scoreBoard.startGame("Germany", "France");
+        scoreBoard.startGame("Uruguay", "Italy");
+        scoreBoard.startGame("Argentina", "Australia");
+
+        scoreBoard.updateScore("Mexico", "Canada", 0, 5);
+        scoreBoard.updateScore("Spain", "Brazil", 10, 2);
+        scoreBoard.updateScore("Germany", "France", 2, 2);
+        scoreBoard.updateScore("Uruguay", "Italy",6, 6);
+        scoreBoard.updateScore("Argentina", "Australia", 3, 1);
+
+        List<Game> games = scoreBoard.getSummary();
+
+        assertEquals("Uruguay", games.get(0).getHomeName());
+        assertEquals("Spain", games.get(1).getHomeName());
+        assertEquals("Mexico", games.get(2).getHomeName());
+        assertEquals("Argentina", games.get(3).getHomeName());
+        assertEquals("Germany", games.get(4).getHomeName());
+    }
 }
