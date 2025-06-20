9fd8e9a76bddef8511a8b650f754659542436b9c
hamxy <zylkadamian@gmail.com>
2025-06-20 21:19:07 +0200

    GREEN: Implement getHomeScore and getAwayScore

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,9 +1,19 @@
 public class Game {
     private final Team home;
     private final Team away;
+    private int homeScore;
+    private int awayScore;
 
     public Game(String home, String away) {
         this.home = new Team(home);
         this.away = new Team(away);
+    }
+
+    public int getHomeScore() {
+        return homeScore;
+    }
+
+    public int getAwayScore() {
+        return awayScore;
     }
 }
